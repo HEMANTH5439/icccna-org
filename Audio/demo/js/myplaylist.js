940380206638a70c3f34ceefa5ebf2e82be8fdf2
ICCCNA Developer <dev@icccna.org>
2026-08-06 17:36:26 -0500

    Initial commit of icccna.org website clone

---
+++ Audio/demo/js/myplaylist.js
@@ -0,0 +1,22 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/Matti pathra.mp3',
+       // oga:'mix/1.ogg',
+        title:'STOTHRAM AUDIO CD',
+        artist:'Dr REGI PAUL',
+       // rating:4,
+       // buy:'#',
+       // price:'0.99',
+        duration:'10:40',
+        cover:'mix/audio-cd.jpg'
+    }
+];
---
+++ js/main.js
@@ -0,0 +1,16 @@
+﻿jQuery(document).ready(function(){ 
+
+jQuery(function(){
+      jQuery('.flexslider-home').flexslider({
+        animation: "slide"        
+      });
+    });	
+	
+jQuery(function(){
+    jQuery('.flexslider-vec').flexslider({
+       animation: "slide",        
+       direction: "vertical"
+     });
+   });
+   	
+});
